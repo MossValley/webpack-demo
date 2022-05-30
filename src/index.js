2efcf25c35886f6fce0dbdcb29cb257bdd6daad8
rami <rami.habib@maramalabs.com>
2022-05-30 15:52:31 +1200

    load data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 import myName from './myName';
 import './style.css';
 import webpackimg from './webpackimg.png';
+import Data from './data.xml';
+import Notes from './data.csv';
 
 function component() {
   const element = document.createElement('div');
@@ -15,6 +17,9 @@ function component() {
   myImg.src = webpackimg;
 
   element.appendChild(myImg);
+
+  console.log(Data);
+  console.log(Notes);
 
   return element;
 }

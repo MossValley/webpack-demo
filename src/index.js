1b793254612edee6ff4014de996ec469f7390749
rami <rami@adrastea.fritz.box>
2022-05-30 14:02:59 +1200

    style: indentation to 2 spaces instead of 4

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import _ from 'lodash';
 import myName from './myName';
 
 function component() {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
+  return element;
 }
 
 function naming(n) {
-    const element = document.createElement('div');
+  const element = document.createElement('div');
 
-    element.textContent = myName(n);
-    return element;
+  element.textContent = myName(n);
+  return element;
 }
 
 document.body.appendChild(component());

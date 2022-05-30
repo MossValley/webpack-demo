5a0543b5053910a0c76e047b1b3754cdf03d8f25
rami <rami.habib@maramalabs.com>
2022-05-31 10:05:05 +1200

    add htmlwebpack plugin

--- src/index.js
+++ src/index.js
@@ -1,47 +1,18 @@
 import _ from 'lodash';
-import myName from './myName';
-import './style.css';
-import webpackimg from './webpackimg.png';
-import Data from './data.xml';
-import Notes from './data.csv';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title);
-console.log(toml.owner.name);
-
-console.log(yaml.title);
-console.log(yaml.owner.name);
-
-console.log(json.title);
-console.log(json.owner.name);
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // Add the image to our existing div
-  const myImg = new Image();
-  myImg.src = webpackimg;
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
 
-  element.appendChild(myImg);
+  element.appendChild(btn);
 
-  console.log(Data);
-  console.log(Notes);
-
-  return element;
-}
-
-function naming(n) {
-  const element = document.createElement('div');
-
-  element.textContent = myName(n);
   return element;
 }
 
 document.body.appendChild(component());
-document.body.appendChild(naming('Cody'));

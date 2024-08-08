16890b0b727554965ef5eb7f4fd684a35f032b43
rami <rami.habib@maramalabs.com>
2024-08-08 17:00:23 +1200

    feat: button loader -wip

---
+++ src/buttonLoader.js
@@ -0,0 +1,23 @@
+import _ from 'lodash';
+import printMe from './print';
+
+function component() {
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
+
+  return element;
+}
+
+export default function button() {
+  document.body.appendChild(component());
+};
+
+
+
